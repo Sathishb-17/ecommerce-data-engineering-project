0ca022afbe8b943704d45bc9dabddf0a443da3a0
Mit Patel <pmeet9338@gmail.com>
2026-06-23 17:36:20 +0530

    Initial Data Engineering Project

---
+++ README.md
@@ -0,0 +1,33 @@
+# E-Commerce Data Engineering Project
+
+## Overview
+This project demonstrates a basic ETL pipeline using Python, Pandas, SQLite, and SQLAlchemy.
+
+## Workflow
+
+1. Extract data from CSV
+2. Clean and transform data
+3. Load data into SQLite database
+4. Generate reports
+
+## Technologies Used
+
+- Python
+- Pandas
+- SQLite
+- SQLAlchemy
+- Git
+
+## Project Structure
+
+dataset/
+database/
+scripts/
+reports/
+logs/
+
+## How to Run
+
+pip install -r requirements.txt
+
+python scripts/etl_pipeline.py
---
+++ scripts/etl_pipeline.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from sqlalchemy import create_engine
+import sqlite3
+
+df = pd.read_csv(
+    "dataset/ecommerce_sales.csv"
+)
+
+engine = create_engine(
+    "sqlite:///database/ecommerce.db"
+)
+
+df.to_sql(
+    "sales",
+    engine,
+    if_exists="replace",
+    index=False
+)
+
+print("Database created successfully")
+
+
+
+
+conn = sqlite3.connect(
+    "database/ecommerce.db"
+)
+
+query = """
+SELECT *
+FROM sales
+LIMIT 5
+"""
+
+result = pd.read_sql(query, conn)
+
+print(result)
+
+conn.close()
